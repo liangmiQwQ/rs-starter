476f9848f65282be499b348c57dabc0e4f8ab18c
Liang Mi <github@liangmi.dev>
2026-01-09 21:36:44 +0800

    fix: remove inline git rempo

---
+++ crates/example/src/lib.rs
@@ -0,0 +1,17 @@
+#[must_use]
+pub const fn add(left: u64, right: u64) -> u64 {
+  left + right
+}
+
+#[cfg(test)]
+mod tests {
+  use insta::assert_snapshot;
+
+  use super::*;
+
+  #[test]
+  fn it_works() {
+    let result = add(2, 2);
+    assert_snapshot!(result);
+  }
+}
